6771a645fbcc94f5b170ed441452cbe10f908649
alerciosilva9 <silvaalercio@gmail.com>
2023-05-29 19:03:41 -0300

    Incializando Projeto e criando entidade Costumer

---
+++ src/main/kotlin/com/app/credit/application/entity/Customer.kt
@@ -0,0 +1,7 @@
+package com.app.credit.application.entity
+
+class Customer {
+    var firstName:String=" ";
+    var lastName:String=" ";
+
+}
---
+++ src/test/kotlin/com/app/credit/application/CreditApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.credit.application
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
